e6aeda78bdcbb04c66ea69ab538e4b58198a80fb
Kenzo Chang <kchang@futuredial.com>
2018-10-13 12:02:51 -0700

    Initial

---
+++ TempleteMatchingTest/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TempleteMatchingTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ZoomPanROIPictureBox_OriginalImage = new LibUtility.ZoomPanROIPictureBox();
+            this.ZoomPanROIPictureBox_ProcessedImage = new LibUtility.ZoomPanROIPictureBox();
+            this.ZoomPanROIPictureBox_TempleteImage = new LibUtility.ZoomPanROIPictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoomPanROIPictureBox_OriginalImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoomPanROIPictureBox_ProcessedImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoomPanROIPictureBox_TempleteImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1155, 576);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.ZoomPanROIPictureBox_OriginalImage, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.319149F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.68085F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(571, 282);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.ZoomPanROIPictureBox_ProcessedImage, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(580, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.964539F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95.03546F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(572, 282);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(565, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Original Image :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(566, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Processed Image :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.ZoomPanROIPictureBox_TempleteImage, 0, 1);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 291);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 2;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(571, 282);
+            this.tableLayoutPanel4.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Templete Image:";
+            // 
+            // ZoomPanROIPictureBox_OriginalImage
+            // 
+            this.ZoomPanROIPictureBox_OriginalImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ZoomPanROIPictureBox_OriginalImage.Location = new System.Drawing.Point(3, 18);
+            this.ZoomPanROIPictureBox_OriginalImage.Name = "ZoomPanROIPictureBox_OriginalImage";
+            this.ZoomPanROIPictureBox_OriginalImage.Size = new System.Drawing.Size(565, 261);
+            this.ZoomPanROIPictureBox_OriginalImage.TabIndex = 1;
+            this.ZoomPanROIPictureBox_OriginalImage.TabStop = false;
+            // 
+            // ZoomPanROIPictureBox_ProcessedImage
+            // 
+            this.ZoomPanROIPictureBox_ProcessedImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ZoomPanROIPictureBox_ProcessedImage.Location = new System.Drawing.Point(3, 17);
+            this.ZoomPanROIPictureBox_ProcessedImage.Name = "ZoomPanROIPictureBox_ProcessedImage";
+            this.ZoomPanROIPictureBox_ProcessedImage.Size = new System.Drawing.Size(566, 262);
+            this.ZoomPanROIPictureBox_ProcessedImage.TabIndex = 2;
+            this.ZoomPanROIPictureBox_ProcessedImage.TabStop = false;
+            // 
+            // ZoomPanROIPictureBox_TempleteImage
+            // 
+            this.ZoomPanROIPictureBox_TempleteImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ZoomPanROIPictureBox_TempleteImage.Location = new System.Drawing.Point(3, 144);
+            this.ZoomPanROIPictureBox_TempleteImage.Name = "ZoomPanROIPictureBox_TempleteImage";
+            this.ZoomPanROIPictureBox_TempleteImage.Size = new System.Drawing.Size(279, 135);
+            this.ZoomPanROIPictureBox_TempleteImage.TabIndex = 3;
+            this.ZoomPanROIPictureBox_TempleteImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 576);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoomPanROIPictureBox_OriginalImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoomPanROIPictureBox_ProcessedImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoomPanROIPictureBox_TempleteImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Label label3;
+        private LibUtility.ZoomPanROIPictureBox ZoomPanROIPictureBox_OriginalImage;
+        private LibUtility.ZoomPanROIPictureBox ZoomPanROIPictureBox_ProcessedImage;
+        private LibUtility.ZoomPanROIPictureBox ZoomPanROIPictureBox_TempleteImage;
+    }
+}
+
---
+++ TempleteMatchingTest/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TempleteMatchingTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void test()
+        {
+            Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte> m_Image = new Emgu.CV.Image<Emgu.CV.Structure.Bgr, byte>(@"C:\Users\Public\Documents\MVTec\HALCON-13.0\examples\images\color\clamp_pile_03.png");
+
+            //ZoomPanROIPictureBox_OriginalImage.Image
+
+        }
+    }
+}
